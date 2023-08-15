48ad9d9f4024e1ea1f1ddf913cea1e12b9eb89d3
coreORB <rafal.ziemlanka@gmail.com>
2023-08-15 14:11:17 +0200

    Added encoding, separator to test inputs/outputs.

--- lib/input.dart
+++ lib/input.dart
@@ -1,30 +1,33 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:detete/configure.dart';
 
-/// Reads file at [filepath] as list of lines.
+/// Reads file at [filepath] as string using the given [Encoding].
 ///
 /// Set [skipInCliArgs] to _true_ to disallow changing [filepath] set in code
 /// when script is run from command line.
 String readAsString({
   String filepath = "inputs/input.txt",
   bool skipInCliArgs = false,
+  Encoding encoding = utf8,
 }) {
   filepath = getInputPathFromArgs(filepath, skipInCliArgs);
-  return File(filepath).readAsStringSync();
+  return File(filepath).readAsStringSync(encoding: encoding);
 }
 
-/// Reads file at [filepath] as text.
+/// Reads file at [filepath] as list of lines using the given [Encoding].
 ///
 /// Set [skipInCliArgs] to _true_ to disallow changing [filepath] set in code
 /// when script is run from command line.
 List<String> readAsLines({
   String filepath = "inputs/input.txt",
   bool skipInCliArgs = false,
+  Encoding encoding = utf8,
 }) {
   filepath = getInputPathFromArgs(filepath, skipInCliArgs);
-  return File(filepath).readAsLinesSync();
+  return File(filepath).readAsLinesSync(encoding: encoding);
 }
 
 /// Reads file at [filepath] as bytes.
--- lib/output.dart
+++ lib/output.dart
@@ -1,23 +1,25 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
 import 'configure.dart';
 
 extension OutputTextFile on String {
-  /// Writes file at [filepath] with [this] as text.
+  /// Writes file at [filepath] with [this] as text using the given [Encoding].
   ///
   /// Set [skipInCliArgs] to _true_ to disallow changing [filepath] set in code
   /// when script is run from command line.
   void writeAsString({
     String filepath = "outputs/output.txt",
     bool skipInCliArgs = false,
+    Encoding encoding = utf8,
   }) {
     filepath = getOutputPathFromArgs(filepath, skipInCliArgs);
-    File(filepath).writeAsStringSync(this);
+    File(filepath).writeAsStringSync(this, encoding: encoding);
   }
 }
 
-/// Writes file at [filepath] with [content] as text.
+/// Writes file at [filepath] with [content] as text using the given [Encoding].
 ///
 /// Set [skipInCliArgs] to _true_ to disallow changing [filepath] set in code
 /// when script is run from command line.
@@ -25,37 +27,47 @@ void writeAsString(
   String content, {
   String filepath = "outputs/output.txt",
   bool skipInCliArgs = false,
+  Encoding encoding = utf8,
 }) {
-  content.writeAsString(filepath: filepath, skipInCliArgs: skipInCliArgs);
+  content.writeAsString(
+      filepath: filepath, skipInCliArgs: skipInCliArgs, encoding: encoding);
 }
 
 extension OutputTextFileLinesIterable<T> on Iterable<T> {
-  /// Writes file at [filepath] with [this] as text.
+  /// Writes file at [filepath] with [this] joined to text using [separator]
+  /// and using the given [Encoding].
   ///
   /// Set [skipInCliArgs] to _true_ to disallow changing [filepath] set in code
   /// when script is run from command line.
-  void writeListAsString({
+  void writeIterableAsString({
     String filepath = "outputs/output.txt",
     bool skipInCliArgs = false,
+    Encoding encoding = utf8,
     String separator = "\n",
   }) {
     filepath = getOutputPathFromArgs(filepath, skipInCliArgs);
-    File(filepath).writeAsStringSync(join(separator));
+    File(filepath).writeAsStringSync(join(separator), encoding: encoding);
   }
 }
 
-/// Writes file at [filepath] with [content] as text.
+/// Writes file at [filepath] with [content] joined to text using [separator]
+/// and using the given [Encoding].
 ///
 /// Set [skipInCliArgs] to _true_ to disallow changing [filepath] set in code
 /// when script is run from command line.
-void writeListAsString<T>(
+void writeIterableAsString<T>(
   Iterable<T> content, {
   String filepath = "outputs/output.txt",
   bool skipInCliArgs = false,
+  Encoding encoding = utf8,
   String separator = "\n",
 }) {
-  content.writeListAsString(
-      filepath: filepath, skipInCliArgs: skipInCliArgs, separator: separator);
+  content.writeIterableAsString(
+    filepath: filepath,
+    skipInCliArgs: skipInCliArgs,
+    separator: separator,
+    encoding: encoding,
+  );
 }
 
 extension OutputBytesFile on Uint8List {
