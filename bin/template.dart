8e80cba3c38287318b8604fad0b4a2f012dde181
coreORB <rafal.ziemlanka@gmail.com>
2023-08-15 13:38:21 +0200

    added template.dart

---
+++ bin/template.dart
@@ -0,0 +1,13 @@
+import 'package:detete/configure.dart';
+import 'package:detete/input.dart' as input;
+import 'package:detete/output.dart' as output;
+import 'package:detete/utils.dart' as utils;
+
+void main(List<String> arguments) {
+  parseArgs(arguments);
+  transform();
+}
+
+void transform() {
+  //your code here
+}
